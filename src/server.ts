390f144f106e7efce503ea7b51f4d028ee9fc411
nanderLP <nander@nander.dev>
2022-08-13 17:00:02 +0200

    remove twitter client it seems like there's not a fully functional twitter library yet, but I can live with making the API request on my own

--- src/server.ts
+++ src/server.ts
@@ -5,17 +5,10 @@
 // web server (oak)
 import { Application, Router } from "oak";
 
-// twitter api (twitter-api-sdk)
-import { Client } from "twitter";
-
 // -- twitter --
 
 // load twitter api bearer token
 const twitterToken = Deno.env.get("TWITTER_BEARER");
-
-// create twitter client
-if (!twitterToken) throw new Error("Missing twitter bearer token");
-const twitter = new Client(twitterToken);
 
 // -- server --
 
@@ -31,16 +24,24 @@ router.get("/", (ctx) => {
 });
 
 // custom route (/*), this "route" won't be handled by the router, because trying to match an url-path with the router regex is very difficult
-app.use((ctx, next) => {
+app.use(async (ctx, next) => {
   // try to parse the path as a url
   // if it works then it's the dynamic route, which should parse the tweet and return the video file
   // if it doesn't then just continue the middleware chain
   try {
     const url = new URL(ctx.request.url.pathname.substring(1));
 
-    // see issue #1
     const pathSegments = url.pathname.split("/");
-    console.log(pathSegments);
+
+    // see issue #1
+    const tweetId = pathSegments.find((v, i, o) =>
+      /^\d+$/.test(v) && o[i - 1] === "status"
+    );
+
+    // ignore request if no tweet id was found
+    if (!tweetId) return next();
+
+    // get tweet video link
   } catch {
     next();
   }
